3dd04a3ef3738ab2c9449ef6ba52e1b3aa4d9b05
Anton Chernov <chernov.anton.mail@gmail.com>
2018-04-05 11:09:16 +0300

    void setX(const int &x) void setY(const int &y) are added in MyClass
    	modified:   myclass.cpp
    	modified:   myclass.h

--- myclass.cpp
+++ myclass.cpp
@@ -1,5 +1,13 @@
 #include "myclass.h"
-int myclass::set()
+void myclass::setX(const int &x)
 {
-    return 0;
+    if(x>0)
+        _x=x;
+    else
+        _x=-x;
+}
+
+void myclass::setY(const int &y)
+{
+    _y=(y<0)?y:(-y);
 }
--- myclass.h
+++ myclass.h
@@ -1,8 +1,12 @@
 #ifndef MYCLASS_H
 #define MYCLASS_H
 class myclass{
+protected:
+    int _x;
+    int _y;
 public:
-    int set();
+    void setX(const int &x);
+    void setY(const int &y);
 };
 
 #endif // MYCLASS_H
